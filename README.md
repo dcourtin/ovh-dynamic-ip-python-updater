3d8b5b8160f6c597a8a23c7218064199628929cb
David Courtin <courtin.david@gmail.com>
2023-10-22 22:39:40 +0200

    add files

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Fetch an IP adress and update an OVH dynamic domain 
 
 Make it executable and run in scheluded task once in hour.
+
+./ovh-dyndomain-updater.py --ovhdynid={ovh id} --ovhdynpass={password} --domain={domain}
---
+++ ovh-dyndomain-updater.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import argparse
+import requests
+import sys
+
+parser = argparse.ArgumentParser(description='Update OVH Dynamic IP')
+parser.add_argument('--ovhdynid', 
+                    help='OVH Id tied to domain', required=True)
+parser.add_argument('--ovhdynpass',
+                    help='OVH Password tied to domain', required=True)
+parser.add_argument('--domain', 
+                    help='OVH DynDomain', required=True)
+
+def get_ip():
+    try:
+        ip_response  = requests.get('https://ident.me')
+        ip_response.raise_for_status()
+        ip = ip_response.text
+        print('IP: '+ip)
+        return ip
+    except:
+        print('Error getting IP')
+        sys.exit(1)
+
+def update_domain(ip, id, password, domain):
+    try:
+   # req = 'https://www.ovh.com/nic/update?system=dyndns&hostname='+dyndomain+'&myip='+ip
+
+        ovh_url = 'https://%s:%s@www.ovh.com/nic/update?system=dyndns&hostname=%s&myip=%s' %  (id, password, domain, ip)
+        print(ovh_url)
+        update_domain_response = requests.get(ovh_url)
+        update_domain_response.raise_for_status()
+        print(update_domain_response.text)
+    except:
+        print('Error updating domain')
+        sys.exit(1)
+
+try:
+    args       = parser.parse_args()
+    ovhdynid   = args.ovhdynid
+    ovhdynpass = args.ovhdynpass
+    domain     = args.domain   
+
+    ip=get_ip()
+    update_domain(ip=ip, id=ovhdynid, password=ovhdynpass, domain=domain)
+
+except:
+    exception = sys.exc_info()
+    print(exception)
+    parser.print_help()
+    sys.exit(1)
